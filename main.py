11b60faa69a0c6c55259d2cc65b01c56b8ee2b3f
Kavindu <kmvithanage48@gmail.com>
2024-05-16 23:51:48 +0530

    updates

--- main.py
+++ main.py
@@ -8,9 +8,12 @@ options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=options)
 wait = WebDriverWait(driver, 10)
 driver.get("https://mail.google.com")
+driver.maximize_window()
 
 email = wait.until(EC.visibility_of_element_located((By.ID, "identifierId")))
-email.send_keys("Test")
+email.send_keys("kavindu@ceydigital.com")
+next_btn = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'button[class="VfPpkd-LgbsSe VfPpkd-LgbsSe-OWXEXe-Bz112c-M1Soyc VfPpkd-LgbsSe-OWXEXe-dgl2Hf ksBjEc lKxP2d LQeN7 BqKGqe eR0mzb TrZEUc lw1w4b"] span[class="VfPpkd-vQzf8d"]')))
+next_btn.click()
 
 
 
