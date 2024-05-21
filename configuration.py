e345102fbf3bf30e315afa60ba13befc01954a14
Kavindu <kmvithanage48@gmail.com>
2024-05-21 20:25:10 +0530

    updates

---
+++ configuration.py
@@ -0,0 +1,2 @@
+BaseURL = "https://mail.google.com"
+Email = "kavindu@ceydigital.com"  # update your email
--- main.py
+++ main.py
@@ -5,18 +5,19 @@ from selenium.common import NoSuchElementException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import configuration
 
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=options)
 wait = WebDriverWait(driver, 20)
-driver.get("https://mail.google.com")
+driver.get(configuration.BaseURL)
 driver.maximize_window()
 
 
 def enter_email():
     email = wait.until(EC.visibility_of_element_located((By.ID, "identifierId")))
-    email.send_keys("kavindu@ceydigital.com")
+    email.send_keys(configuration.Email)
     next_btn = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR,
                                                             'button[class="VfPpkd-LgbsSe VfPpkd-LgbsSe-OWXEXe-k8QpJ VfPpkd-LgbsSe-OWXEXe-dgl2Hf nCP5yc AjY5Oe DuMIQc LQeN7 BqKGqe Jskylb TrZEUc lw1w4b"] span[class="VfPpkd-vQzf8d"]')))
     next_btn.click()
